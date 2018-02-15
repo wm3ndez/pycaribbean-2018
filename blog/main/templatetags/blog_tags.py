9cb659e38bc35ac11699719716701457a1443299
Williams Mendez <wmendez27@gmail.com>
2018-02-15 10:51:22 -0400

    Add a Category tree to the sidebar

--- blog/main/templatetags/blog_tags.py
+++ blog/main/templatetags/blog_tags.py
@@ -1,8 +1,16 @@
+from collections import namedtuple
+
 from django import template
+from django.core.cache import cache
 from django.db.models import F, Count, FloatField
 from django.db.models.functions import Cast
 
+from blog.main.models import Category
+
 register = template.Library()
+COMMENTS_FACTOR = 0.1
+VIEWS_FACTOR = 0.01
+CategoryTree = namedtuple('CategoryTree', ['id', 'name', 'children'])
 
 
 @register.inclusion_tag('pagination.html')
@@ -14,9 +22,10 @@ def paginate(page_obj):
 def popular_posts(PostModel, slice=5):
     Post = PostModel.__class__
     posts = Post.objects.prefetch_related('postcomment_set')
-    posts = posts.annotate(
-        post_score=Cast(F('views') * 0.01 + Count('postcomment') * 0.1, FloatField()),
-    ).order_by('-post_score')
+
+    score = F('views') * VIEWS_FACTOR + Count('postcomment') * COMMENTS_FACTOR
+    score = Cast(score, FloatField())
+    posts = posts.annotate(post_score=score).order_by('-post_score')
 
     return {'posts': posts[:slice]}
 
@@ -26,3 +35,38 @@ def featured_posts(PostModel, slice=5):
     Post = PostModel.__class__
     posts = Post.objects.filter(featured=True)
     return {'posts': posts[:slice]}
+
+
+@register.inclusion_tag('category-tree.html')
+def category_tree():
+    # @cached
+    def get_children(category):
+        return [CategoryTree(c.id, c.name, get_children(c))
+                for c in category.category_set.all()]
+
+    # Parent categories
+    categories = Category.objects.filter(parent__isnull=True)
+    tree = []
+    for category in categories:
+        # children = cache.get('category-%s' % category.id)
+        children = None
+        if children is None:
+            children = get_children(category)
+            cache.set(
+                'category-%s' % category.id,
+                children,
+                timeout=60 * 60 * 3  # 3 hours
+            )
+
+        tree.append(CategoryTree(category.id, category.name, children))
+
+    return {'tree': _render_category_tree(tree)}
+
+
+def _render_category_tree(tree):
+    html = '<ul>'
+    for node in tree:
+        html += '<li><a href="#">{}</a>{}</li>'.format(node.name,
+                                                       _render_category_tree(node.children))
+    html += '</ul>'
+    return html
