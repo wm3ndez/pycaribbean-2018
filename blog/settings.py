7443d99a6e045167acb94a1d27331722517d9f23
Williams Mendez <wmendez27@gmail.com>
2019-02-06 12:33:35 -0400

    Change testing db

--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -89,6 +90,15 @@ DATABASES = {
         'PORT': 5432,
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
